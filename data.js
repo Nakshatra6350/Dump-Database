076911957b2b2f392e6eae77812541666d38591c
Nakshatra6350 <76101097+Nakshatra6350@users.noreply.github.com>
2024-03-14 18:24:08 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,65 @@
+const { MongoClient, ObjectId } = require("mongodb");
+const mysql = require("mysql2/promise");
+
+let connections = [];
+
+async function fetchHostUserPassword(objectId) {
+  const uri =
+    "mongodb+srv://gpulkit09:1SmeJr2eSLlzXWIE@cluster0.f6zqffn.mongodb.net/?retryWrites=true&w=majority";
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+
+    const database = client.db("graviton_microservice");
+    const collection = database.collection("dumpConnectionDetails");
+
+    const query = { _id: new ObjectId(objectId) };
+    const projection = { host: 1, user: 1, password: 1, _id: 0 };
+
+    const result = await collection.findOne(query, { projection });
+
+    if (result) {
+      const { host, user, password } = result;
+
+      const sqlConnection = await mysql.createConnection({
+        host: host,
+        user: user,
+        password: password,
+      });
+
+      const [rows] = await sqlConnection.execute("SHOW DATABASES");
+
+      const databases = rows
+        .map((row) => row.Database)
+        .filter(
+          (dbName) =>
+            dbName !== "information_schema" && dbName !== "performance_schema"
+        )
+        .map((dbName) => ({
+          host: host,
+          user: user,
+          password: password,
+          dbName: dbName,
+        }));
+      await sqlConnection.end();
+
+      console.log("Host, User, and Password are:", { host, user, password });
+      console.log("Databases:", databases);
+
+      connections = databases;
+      console.log("Array of objects of data : ", connections);
+      return connections;
+    } else {
+      console.log("Entry not found");
+      return [];
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return [];
+  } finally {
+    await client.close();
+  }
+}
+
+module.exports = fetchHostUserPassword;
---
+++ index.js
@@ -0,0 +1,94 @@
+const mysql = require("mysql2/promise");
+const mysqldump = require("mysqldump");
+const cron = require("node-cron");
+const fs = require("fs");
+const path = require("path");
+const fetchHostUserPassword = require("./data.js");
+
+const dir = "./databaseDumps";
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir);
+}
+
+cron.schedule(
+  "0 20 * * *",
+  async () => {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+    const day = String(currentDate.getDate()).padStart(2, "0");
+    const dateFolderName = `${year}-${month}-${day}`;
+    const time = `${currentDate.getHours()}-${currentDate.getMinutes()}-${
+      currentDate.getSeconds
+    }`;
+    const folderPath = `${dir}/dump_${dateFolderName}_${time}`;
+
+    async function dumpData(connectionObj) {
+      const connection = await mysql.createConnection({
+        host: connectionObj.host,
+        user: connectionObj.user,
+        password: connectionObj.password,
+        database: connectionObj.dbName,
+      });
+
+      try {
+        console.log(`Connected to MySQL database ${connectionObj.dbName}`);
+
+        if (!fs.existsSync(folderPath)) {
+          fs.mkdirSync(folderPath);
+          console.log(`Folder created: ${folderPath}`);
+        }
+
+        await mysqldump({
+          connection: {
+            host: connectionObj.host,
+            user: connectionObj.user,
+            password: connectionObj.password,
+            database: connectionObj.dbName,
+          },
+          dumpToFile: `${folderPath}/${connectionObj.host}_${connectionObj.dbName}.sql`,
+        });
+
+        console.log(`Database ${connectionObj.dbName} dumped successfully`);
+      } catch (err) {
+        console.error(`Error dumping database ${connectionObj.dbName}:`, err);
+      } finally {
+        await connection.end();
+      }
+    }
+
+    async function deleteFolder() {
+      const dumpFolder = await fs.promises.readdir(dir);
+      if (dumpFolder.length > 7) {
+        const oldestFolder = dumpFolder[0];
+        const oldestFolderPath = path.join(dir, oldestFolder);
+        await fs.promises.rm(oldestFolderPath, { recursive: true });
+        console.log(`Folder deleted: ${oldestFolderPath}`);
+      } else {
+        console.log("less than or equal to 7 file");
+      }
+    }
+
+    async function dumpAllData() {
+      objectIds = ["65e9764cb3dcbd32db1542f3", "65e99126b3dcbd32db1542f4"];
+
+      for (let i = 0; i < objectIds.length; i++) {
+        // Fixed the loop condition
+        const connections = await fetchHostUserPassword(objectIds[i]);
+        for (const connectionObj of connections) {
+          await dumpData(connectionObj);
+        }
+      }
+    }
+
+    console.log(
+      "-----------------------------------------------------------------"
+    );
+    await dumpAllData();
+    console.log("..........................DELETION........................");
+    await deleteFolder();
+  },
+  {
+    timezone: "Asia/Kolkata",
+  }
+);
