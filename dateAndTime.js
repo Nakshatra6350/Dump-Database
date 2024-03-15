f01381b4612fde31be9c745ae5fc0997e401876a
Nakshatra6350 <76101097+Nakshatra6350@users.noreply.github.com>
2024-03-15 18:26:13 +0530

    Update dateAndTime.js

--- dateAndTime.js
+++ dateAndTime.js
@@ -4,8 +4,8 @@ const getFolderPath = () => {
   const month = String(currentDate.getMonth() + 1).padStart(2, "0");
   const day = String(currentDate.getDate()).padStart(2, "0");
   const dateFolderName = `${year}-${month}-${day}`;
-  const time = `${currentDate.getHours()}-${currentDate.getMinutes()}-${currentDate.getSeconds()}`;
-  const folderPath = `${time}`;
+  
+  const folderPath = `${dateFolderName}`;
 
   return folderPath;
 };
