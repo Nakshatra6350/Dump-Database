16bb0b13e27f4ce06e26857dab9c3d667f9531ed
Nakshatra6350 <76101097+Nakshatra6350@users.noreply.github.com>
2024-03-15 18:19:52 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,19 +1,19 @@
 const { MongoClient, ObjectId } = require("mongodb");
 const mysql = require("mysql2/promise");
+const path = require("path");
+const fs = require("fs");
+require("dotenv").config();
 
 let connections = [];
+const logPath = ["logs/success.txt", "logs/error.txt"];
 
-async function fetchHostUserPassword(objectId) {
-  const uri =
-    "your_mongodb_uri";
-  const client = new MongoClient(uri);
+const fetchHostUserPassword = async (objectId) => {
+  const client = new MongoClient(process.env.MONGO_URI);
 
   try {
     await client.connect();
-
-    const database = client.db("graviton_microservice");
-    const collection = database.collection("dumpConnectionDetails");
-
+    const database = client.db(process.env.DB_NAME);
+    const collection = database.collection(process.env.COLLECTION_NAME);
     const query = { _id: new ObjectId(objectId) };
     const projection = { host: 1, user: 1, password: 1, _id: 0 };
 
@@ -44,22 +44,47 @@ async function fetchHostUserPassword(objectId) {
         }));
       await sqlConnection.end();
 
-      console.log("Host, User, and Password are:", { host, user, password });
-      console.log("Databases:", databases);
+      const logsFolder = path.dirname(logPath[0]);
+      if (!fs.existsSync(logsFolder)) {
+        fs.mkdirSync(logsFolder, { recursive: true });
+        console.log(`Logs folder created: ${logsFolder}`);
+      }
 
+      fs.appendFileSync(
+        logPath[0],
+        `Host, User, and Password are: ${JSON.stringify(
+          {
+            host,
+            user,
+            password,
+          },
+          null,
+          2
+        )}\n`
+      );
       connections = databases;
-      console.log("Array of objects of data : ", connections);
+
+      fs.appendFileSync(
+        logPath[0],
+        `Array of objects containing host,user,password and database name for each database: ${JSON.stringify(
+          connections,
+          null,
+          2
+        )}\n`
+      );
       return connections;
     } else {
-      console.log("Entry not found");
+      fs.appendFileSync(logPath[0], "objectId not found in mongo\n");
       return [];
     }
   } catch (error) {
-    console.error("Error fetching data:", error);
+    fs.appendFileSync(
+      logPath[1],
+      `Error fetching data from MongoDB : ${JSON.stringify(error)}`
+    );
     return [];
   } finally {
     await client.close();
   }
-}
-
+};
 module.exports = fetchHostUserPassword;
